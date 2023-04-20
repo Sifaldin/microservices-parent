e5814809f4b6842c4e1a63b64d070e84dd4f5cd0
Sif Aldin Abbas <sifaldin.abbas@kindredgroup.com>
2023-04-21 00:49:31 +0200

    add RestTemplate - add loggings

--- fraud/src/main/java/com/abbas/fraud/FraudController.java
+++ fraud/src/main/java/com/abbas/fraud/FraudController.java
@@ -1,6 +1,7 @@
 package com.abbas.fraud;
 
 import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("api/v1/fraud-check")
 @AllArgsConstructor
+@Slf4j
 public class FraudController {
     private final FraudService fraudService;
 
@@ -16,6 +18,8 @@ public class FraudController {
     public FraudCheckResponse isFraudster(
             @PathVariable("customerId") Integer customerId) {
         boolean isFraudulentCustomer = fraudService.isFraudulent(customerId);
+        log.info("fraud check request for customer {}", customerId);
+
         return new FraudCheckResponse(isFraudulentCustomer);
     }
 }
